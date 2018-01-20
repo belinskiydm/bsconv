99caedc0960e6afea85ac00bb49c6bcfe2790d76
DmitriyBelinskiy <dmitriybelinskiy@gmail.com>
2018-01-20 17:51:06 +0300

    total code refactoring

--- baseconverter.go
+++ baseconverter.go
@@ -1,10 +1,11 @@
-// Package baseconverter converts number from an arbitrary base to another arbitrary base.
-// From binary up to base62.
+// Package baseconverter converts any number from an arbitrary base to another arbitrary base.
+// From binary up to base62. Not only from base2 to base36 like in standard library. The package
+// also contains two additional functions to convert from an arbitrary base to decimal and from
+// decimal to an arbitrary base.
 package baseconverter
 
 import (
 	"fmt"
-	"log"
 	"math"
 	"strconv"
 )
@@ -12,82 +13,85 @@ import (
 // List of all digits from base2 to base62.
 const allDigits = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-// Flag of the negativ number.
+// Flag of the negative number.
 var flagNeg uint
 
 // Conversion converts number from an arbitrary base to another arbitrary base.
 func Conversion(toConv string, fromBase, toBase int) (string, error) {
-	// Basic error checkings.
-	if fromBase < 2 || toBase < 2 {
-		return "error", fmt.Errorf("Invalid base: %v or %v", fromBase, toBase)
-	}
-	if len(toConv) == 0 {
-		return "error", fmt.Errorf("Nothing to convert")
-	}
-	if fromBase == toBase {
-		return toConv, nil
-	}
-	// If the number is negativ: flagNeg = 1 and "saving" the negative sign.
-	numS := []rune(toConv)
-	if numS[0] == rune('-') {
-		flagNeg = 1
-		numS = append(numS[1:])
-	}
-	// Checking if number exists and belongs to the declared base(fromBase).
-	if len(numS) == 0 || numIsValid(numS, fromBase) != true {
-		return "error", fmt.Errorf("Invalid number: %v", toConv)
-	}
 	// Converting the number to decimal first.
-	decNum := ConvertToDec(numS, fromBase)
+	decNum, err := ConvertToDec(toConv, fromBase)
+	if err != nil {
+		return "", err
+	}
 	// Converting the number from decimal to the declared base(toBase).
-	newBaseNum := ConvertFromDec(decNum, toBase)
-
+	newBaseNum, err := ConvertFromDec(decNum, toBase)
+	if err != nil {
+		return "", err
+	}
 	return newBaseNum, nil
 }
 
 // ConvertToDec converts an arbitrary base number to decimal base number.
-func ConvertToDec(num []rune, fromBase int) float64 {
-	// If number base is already decimal - no need to convert.
-	if fromBase == 10 {
-		numF, err := strconv.ParseFloat(string(num), 64)
-		if err != nil {
-			log.Fatalln(err)
-		}
-		return numF
+func ConvertToDec(numS string, fromBase int) (string, error) {
+	if fromBase < 2 {
+		return "", fmt.Errorf("Invalid base: %v", fromBase)
 	}
+	if numS == "" {
+		return "", fmt.Errorf("Nothing to convert")
+	}
+
+	// If the number is negative: flagNeg = 1 and "saving" the negative sign.
+	num := []rune(numS)
+	if num[0] == rune('-') {
+		flagNeg = 1
+		num = append(num[1:])
+	}
+
 	// Converting the number to decimal base.
-	var decNum float64
+	var decNum int64
 	iFromEnd := len(num) - 1
 	for _, vRune := range num {
-		vInt := letterToNum(vRune, fromBase)
-		decNum += float64(vInt) * math.Pow(float64(fromBase), float64(iFromEnd))
+		vInt, err := letterToNum(vRune, fromBase)
+		if err != nil {
+			return "", err
+		}
+		decNum += int64(vInt) * int64(math.Pow(float64(fromBase), float64(iFromEnd)))
 		iFromEnd--
 	}
-	return decNum
+	if flagNeg == 1 {
+		decNum *= -1
+	}
+
+	return fmt.Sprint(decNum), nil
 }
 
 // ConvertFromDec converts decimal base number to an arbitrary base number.
-func ConvertFromDec(num float64, toBase int) string {
-	// If toBase is decimal - no need to convert.
-	if toBase == 10 {
-		if flagNeg != 0 {
-			// If original number was negative - making new one negative too.
-			num *= -1
-			flagNeg = 0
-		}
-		return fmt.Sprint(num)
+func ConvertFromDec(numS string, toBase int) (string, error) {
+	if toBase < 2 {
+		return "", fmt.Errorf("Invalid base: %v", toBase)
+	}
+	if numS == "" {
+		return "", fmt.Errorf("Nothing to convert")
+	}
+	// Converting string to int64
+	num, err := strconv.ParseInt(numS, 10, 64)
+	if err != nil {
+		return "", fmt.Errorf("Invalid number: %v", numS)
 	}
 	// If original number is equal to "0...0".
 	if num == 0 {
-		return "0"
+		return "0", nil
 	}
-
+	// If the number is negativ: flagNeg = 1 and "saving" the negative sign.
+	if num < 0 {
+		flagNeg = 1
+		num *= -1
+	}
 	// Converting the number to needed base (toBase).
 	var newNum string
 	for num != 0 {
-		reminder := math.Mod(num, float64(toBase))
-		num = num / float64(toBase)
-		num, _ = math.Modf(num)
+		reminder := num % int64(toBase)
+		num = num / int64(toBase)
 		reminderHex := numToLetter(reminder, toBase)
 		newNum += reminderHex
 	}
@@ -98,64 +102,42 @@ func ConvertFromDec(num float64, toBase int) string {
 		flagNeg = 0
 	}
 	// Reversing the number.
-	newNum = numReverse([]rune(newNum))
+	numRunes := []rune(newNum)
+	left := 0
+	right := len(numRunes) - 1
+	for left < len(numRunes)/2 {
+		numRunes[left], numRunes[right] = numRunes[right], numRunes[left]
+		left++
+		right--
+	}
 
-	return newNum
+	return string(numRunes), nil
 }
 
 // letterToNum converts rune(it can be digit(0-9) or letter(a-z,A-Z) to int.
-func letterToNum(digit rune, base int) int {
+func letterToNum(digit rune, base int) (int, error) {
 	if base > 10 {
 		for i, v := range allDigits[10:] {
 			if digit == v {
-				return i + 10
+				return i + 10, nil
 			}
 		}
 	}
 	digitInt, err := strconv.Atoi(string(digit))
 	if err != nil {
-		log.Fatal(err)
+		return 0, fmt.Errorf("Invalid digit %v", string(digit))
 	}
-	return digitInt
+	return digitInt, nil
 }
 
-// numToLetter converts float64 digit to string with digits(0-9) or/and letters(a-z,A-Z).
-func numToLetter(digit float64, base int) string {
+// numToLetter converts int64 digit to string with digits(0-9) or/and letters(a-z,A-Z).
+func numToLetter(digit int64, base int) string {
 	if base > 10 {
 		for i, v := range allDigits[10:base] {
-			if int(digit) == i+10 {
+			if digit == int64(i+10) {
 				return string(v)
 			}
 		}
 	}
 	return fmt.Sprint(digit)
-}
-
-// numIsValid checks if number belongs to the declared base.
-func numIsValid(num []rune, checkBase int) bool {
-	for _, v := range num {
-		flagFound := false
-		for _, d := range allDigits[:checkBase] {
-			if v == d {
-				flagFound = true
-				continue
-			}
-		}
-		if flagFound != true {
-			return false
-		}
-	}
-	return true
-}
-
-// numReverse reverses the number.
-func numReverse(num []rune) string {
-	leftSide := 0
-	rightSide := len(num) - 1
-	for leftSide < len(num)/2 {
-		num[leftSide], num[rightSide] = num[rightSide], num[leftSide]
-		leftSide++
-		rightSide--
-	}
-	return string(num)
 }
--- baseconverter_test.go
+++ baseconverter_test.go
@@ -1,4 +1,4 @@
-package baseconvert
+package baseconverter
 
 import (
 	"fmt"
@@ -6,39 +6,96 @@ import (
 )
 
 func TestConversion(t *testing.T) {
-	num, _ := Conversion("0", 2, 10)
-	if num != "0" {
+	//Correct result testing
+	num, err := Conversion("0", 2, 10)
+	if num != "0" && err != nil {
 		t.Error("Expected 0,", "got ", num)
 	}
 
-	num, _ = Conversion("56f", 16, 2)
-	if num != "10101101111" {
+	num, err = Conversion("56f", 16, 2)
+	if num != "10101101111" && err != nil {
 		t.Error("Expected 10101101111,", "got ", num)
 	}
 
-	num, _ = Conversion("-58586655672", 10, 62)
-	if num != "-11WTDte" {
+	num, err = Conversion("-58586655672", 10, 62)
+	if num != "-11WTDte" && err != nil {
 		t.Error("Expected -11WTDte,", "got ", num)
 	}
 
-	num, _ = Conversion("11WTDte", 62, 2)
-	if num != "110110100100000010010110101110111000" {
+	num, err = Conversion("11WTDte", 62, 2)
+	if num != "110110100100000010010110101110111000" && err != nil {
 		t.Error("Expected 110110100100000010010110101110111000,", "got ", num)
+	}
+
+	//Errors testing
+	_, err = Conversion("3334%", 10, 16)
+	if err == nil {
+		t.Error("Expecting an error")
+	}
+	_, err = Conversion("", 10, 16)
+	if err == nil {
+		t.Error("Expecting an error")
+	}
+	_, err = Conversion("", 1, 16)
+	if err == nil {
+		t.Error("Expecting an error")
+	}
+	_, err = Conversion("", 10, 0)
+	if err == nil {
+		t.Error("Expecting an error")
 	}
 
 }
 
 func TestConvertToDec(t *testing.T) {
-	num := ConvertToDec([]rune("453fa"), 16)
-	if num != 283642 {
-		t.Error("Expected 0,", "got ", num)
+	//Correct result testing
+	num, err := ConvertToDec("453fa", 16)
+	if num != "283642" && err != nil {
+		t.Error("Expected 283642,", "got ", num)
+	}
+	num, err = ConvertToDec("-4t3fa", 55)
+	if num != "-41437285" && err != nil {
+		t.Error("Expected -41437285,", "got ", num)
+	}
+
+	//Errors testing
+	_, err = ConvertToDec("453$fa", 16)
+	if err == nil {
+		t.Error("Expecting an error")
+	}
+	_, err = ConvertToDec("", 16)
+	if err == nil {
+		t.Error("Expecting an error")
+	}
+	_, err = ConvertToDec("", -1)
+	if err == nil {
+		t.Error("Expecting an error")
 	}
 }
 
 func TestConvertFromDec(t *testing.T) {
-	num := ConvertFromDec(6455537849492, 62)
-	if num != "1PEvwVPC" {
-		t.Error("Expected 0,", "got ", num)
+	//Correct result testing
+	num, err := ConvertFromDec("-6455537849492", 62)
+	if num != "-1PEvwVPC" && err != nil {
+		t.Error("Expected -1PEvwVPC,", "got ", num)
+	}
+	num, err = ConvertFromDec("57483", 2)
+	if num != "1110000010001011" && err != nil {
+		t.Error("Expected 1110000010001011,", "got ", num)
+	}
+
+	//Errors testing
+	_, err = ConvertFromDec("453$fa", 16)
+	if err == nil {
+		t.Error("Expecting an error")
+	}
+	_, err = ConvertFromDec("", 16)
+	if err == nil {
+		t.Error("Expecting an error")
+	}
+	_, err = ConvertFromDec("", 1)
+	if err == nil {
+		t.Error("Expecting an error")
 	}
 }
 
@@ -49,13 +106,13 @@ func ExampleConversion() {
 }
 
 func ExampleConvertToDec() {
-	fmt.Println(ConvertToDec([]rune("453fa"), 16))
+	fmt.Println(ConvertToDec("453fa", 16))
 	// Output:
 	// 283642
 }
 
 func ExampleConvertFromDec() {
-	fmt.Println(ConvertFromDec(6455537849492, 62))
+	fmt.Println(ConvertFromDec("6455537849492", 62))
 	// Output:
 	// 1PEvwVPC
 }
